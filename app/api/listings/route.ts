5f4d31ce4af881a9fda0f1101d5639b9f673ef28
jbresky <joaquin_bresky@hotmail.com>
2023-08-31 12:50:13 -0300

    listing endpoint - post

---
+++ app/api/listings/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+
+import prisma from '@/app/libs/prismadb'
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+ export async function POST(request: Request){
+
+    const user = await getCurrentUser();
+    
+    if(!user){
+        return NextResponse.error()
+    }
+
+    const body = await request.json();
+
+    const {
+        title,
+        description,
+        imageSrc,
+        category,
+        roomCount,
+        bathroomCount,
+        guestCount,
+        location,
+        price
+    } = body
+
+    const listing = await prisma.listing.create({
+        data: {
+            title,
+            description,
+            imageSrc,
+            category,
+            roomCount,
+            bathroomCount,
+            guestCount,
+            locationValue: location.value,
+            price: parseInt(price, 10),
+            userId: user.id
+        }
+    })
+
+    return NextResponse.json(listing)
+    
+ }
