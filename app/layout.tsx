a2ef0329ac77bee1a24e1b8c95cd5dc5dfe24b2f
jbresky <joaquin_bresky@hotmail.com>
2023-09-07 12:54:22 -0300

    search modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import ClientOnly from './components/ClientOnly';
 import LoginModal from './components/modals/LoginModal';
 import RegisterModal from './components/modals/RegisterModal';
 import RentModal from './components/modals/RentModal';
+import SearchModal from './components/modals/SearchModal';
 import Navbar from './components/navbar/Navbar';
 import './globals.css'
 import ToasterProvider from './providers/ToasterProvider';
@@ -30,6 +31,7 @@ export default async function RootLayout({
         <ClientOnly>
           <ToasterProvider />
           <RentModal />
+          <SearchModal/>
           <LoginModal />
           <RegisterModal />
           <Navbar currentUser={currentUser} />
